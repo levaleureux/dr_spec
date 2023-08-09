5b2dbbf50d1e529f4a4f2aab934817393a3c86a5
levaleureux <133817850+levaleureux@users.noreply.github.com>
2023-08-10 00:27:04 +0200

    Revert "Revert 27 feature/implement sandi meter"

--- spec/matchers_1_spec.rb
+++ spec/matchers_1_spec.rb
@@ -4,10 +4,12 @@ spec :string_matchers do
   #it "checks if a string starts with a specific prefix" do |args, assert|
   it "start_with" do |args, assert|
     expect("Hello, world!").to start_with "Hello"
+    expect("Hello, world!").not_to start_with "hell"
   end
   #it "checks if a string ends with a specific suffix" do |args, assert|
   it "end_with" do |args, assert|
     expect("Hello, world!").to end_with "world!"
+    expect("Hello, world!").not_to end_with "world"
   end
 end
 
@@ -26,24 +28,19 @@ spec :collection_matchers do
   #it "checks if a collection is empty" do |args, assert|
   it "be_empty" do |args, assert|
     expect([]).to be_empty
-    #expect([1, 2, 3]).not_to be_empty
+    expect([1]).not_to be_empty
   end
   #it "checks if a collection contains a specific element" do |args, assert|
   it "contain" do |args, assert|
     expect([1, 2, 3]).to contain 2
-    # expect([1, 2, 3]).not_to contain 4
+    expect([1, 2, 3]).not_to contain 4
   end
-  #it "checks if a collection has a specific size" do |args, assert|
-  it "have_size" do |args, assert|
+  it "checks if a collection has a specific size" do |args, assert|
     expect([1, 2, 3]).to have_size 3
+    expect([1, 2, 3]).not_to have_size 1
   end
-  # it "checks if a collection includes elements in a specific order" do |args, assert|
-  it "include_elements_in_order" do |args, assert|
+  it "checks if a collection includes elements in a specific order" do |args, assert|
     expect([1, 2, 3]).to include_elements_in_order [1, 2, 3]
-  end
-  #it "checks if a collection has a specific size" do |args, assert|
-  it "have_size" do |args, assert|
-    expect([1, 2, 3]).to have_size 3
-    #expect([1, 2, 3]).not_to have_size 4
+    expect([1, 2, 3]).not_to include_elements_in_order [1, 3, 2]
   end
 end
