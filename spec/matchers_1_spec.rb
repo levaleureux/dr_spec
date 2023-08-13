6bfcce6595c7f9200d9b3235905ea00b6b74fa67
Valeureux <operateur1717@gmail.com>
2023-08-13 10:54:41 +0200

    spec can have metadata

--- spec/matchers_1_spec.rb
+++ spec/matchers_1_spec.rb
@@ -1,6 +1,7 @@
 # String Matchers
 #
-spec :string_matchers do
+#focus_spec :string_matchers, tags: [:players] do
+spec :string_matchers, tags: [:players] do
   #it "checks if a string starts with a specific prefix" do |args, assert|
   it "start_with" do |args, assert|
     expect("Hello, world!").to start_with "Hello"
@@ -23,27 +24,20 @@ end
 # Collection Matchers
 #
 spec :collection_matchers do
-  #it "checks if a collection is empty" do |args, assert|
-  it "be_empty" do |args, assert|
-    expect([]).to be_empty
-    #expect([1, 2, 3]).not_to be_empty
+  it "checks if a collection is empty" do |args, assert|
+    expect([]           ).to be_empty
+    expect([1, 2, 3]).not_to be_empty
   end
-  #it "checks if a collection contains a specific element" do |args, assert|
-  it "contain" do |args, assert|
-    expect([1, 2, 3]).to contain 2
-    # expect([1, 2, 3]).not_to contain 4
+  it "checks if a collection contains a specific element" do |args, assert|
+    expect([1, 2, 3]    ).to contain 2
+    expect([1, 2, 3]).not_to contain 4
   end
-  #it "checks if a collection has a specific size" do |args, assert|
-  it "have_size" do |args, assert|
-    expect([1, 2, 3]).to have_size 3
+  it "checks if a collection has a specific size" do |args, assert|
+    expect([1, 2, 3]    ).to have_size 3
+    expect([1, 2, 3]).not_to have_size 4
   end
-  # it "checks if a collection includes elements in a specific order" do |args, assert|
-  it "include_elements_in_order" do |args, assert|
-    expect([1, 2, 3]).to include_elements_in_order [1, 2, 3]
-  end
-  #it "checks if a collection has a specific size" do |args, assert|
-  it "have_size" do |args, assert|
-    expect([1, 2, 3]).to have_size 3
-    #expect([1, 2, 3]).not_to have_size 4
+   it "checks if a collection includes elements in a specific order" do |args, assert|
+    expect([1, 2, 3]    ).to include_elements_in_order [1, 2, 3]
+    expect([2, 1, 3]).not_to include_elements_in_order [1, 2, 3]
   end
 end
